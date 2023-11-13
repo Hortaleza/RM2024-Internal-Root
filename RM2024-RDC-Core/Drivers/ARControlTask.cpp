d9b0b51f0994cb43ff3630d6706bf3c1dee0172c
KarlYu130 <klkyu@connect.ust.hk>
2023-11-13 21:32:13 +0800

    line tracing

--- RM2024-RDC-Core/Drivers/ARControlTask.cpp
+++ RM2024-RDC-Core/Drivers/ARControlTask.cpp
@@ -8,32 +8,39 @@ uint16_t LPIN = GPIO_PIN_7;
 uint16_t RPIN = GPIO_PIN_6;
 static volatile bool status = 0;
 
-float Kp                        = 12;
-float Ki                        = 0.6;
-float Kd                        = 10;
 
-int RPM = 1000;
+static volatile int rpmTest;
+
+float Kp                        = 5;
+float Ki                        = 0;
+float Kd                        = 0;
+
+int RPM = 7500;
 int Lstatus = 0;
 int Rstatus = 0;
 static Control::PID motorPID[2] = {
     {Kp, Ki, Kd}, {Kp, Ki, Kd}
 };
-// id of left motor is 0, right is 1
+// id of left motor is 1, right is 2
 void updateStatus()
 {
     Lstatus = HAL_GPIO_ReadPin(GPIOA, LPIN);
     Rstatus = HAL_GPIO_ReadPin(GPIOA, RPIN);
+   
     // Black: status = 0; White status = 1;
 }
 
 void forward(){
     for (int i = 0; i < 2; i++)
     {
-        
+    
         int targetRPM = RPM;
-        int currentRPM = DJIMotor::motorset[i].getRPM();
-        DJIMotor::motorset[i].setCurrent(
-            motorPID[i].update(targetRPM, currentRPM, 1));
+    int currentRPML = DJIMotor::motorset[0].getRPM();
+        DJIMotor::motorset[0].setCurrent(
+            motorPID[0].update(targetRPM, currentRPML, 1));
+    int currentRPMR = DJIMotor::motorset[1].getRPM();
+        DJIMotor::motorset[1].setCurrent(
+            motorPID[1].update(-targetRPM, currentRPMR, 1));
     }
 }
 
@@ -44,7 +51,7 @@ void left(){
             motorPID[0].update(-targetRPM, currentRPML, 1));
     int currentRPMR = DJIMotor::motorset[1].getRPM();
         DJIMotor::motorset[1].setCurrent(
-            motorPID[1].update(targetRPM, currentRPMR, 1));
+            motorPID[1].update(-targetRPM, currentRPMR, 1));
     
 }
 
@@ -55,7 +62,7 @@ void right(){
             motorPID[0].update(targetRPM, currentRPML, 1));
     int currentRPMR = DJIMotor::motorset[1].getRPM();
         DJIMotor::motorset[1].setCurrent(
-            motorPID[1].update(-targetRPM, currentRPMR, 1));
+            motorPID[1].update(targetRPM, currentRPMR, 1));
     
 }
 
