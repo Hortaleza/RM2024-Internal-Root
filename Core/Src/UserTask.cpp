fe253f631bc1dc142294749bd4d7f8b1b285ddbb
Y-nz <yanning.zhu040927@gmail.com>
2023-11-14 02:46:46 +0800

    Update UserTask.cpp

--- Core/Src/UserTask.cpp
+++ Core/Src/UserTask.cpp
@@ -54,10 +54,10 @@ void ultraSoundTask(void *)
     HAL_TIM_PWM_Start(&htim2,TIM_CHANNEL_2);
     // HAL_TIM_Base_Start(&htim3);
     // HAL_TIM_IC_Start_IT(&htim3,TIM_CHANNEL_1);
-    MG996R::setServoAngle(45);
+    // MG996R::setServoAngle(0);
     while (true)
     {
-        // MG996R::setServoAngle(130);
+        MG996R::setServoAngle(180);
         // distance = HCSR04::HCSR04_Read();
         vTaskDelay(10);  // Delay and block the task for 1ms.
     }
@@ -108,16 +108,16 @@ void startUserTasks()
                       1,
                       uxReceiveTaskStack,
                       &xReceiveTaskTCB);
-    // xTaskCreateStatic(
-    //     ARTask, "ARTask", 256, NULL, 1, uxARTaskStack, &xARTaskTCB);
-    // xTaskCreateStatic(
-    //     ultraSoundTask,
-    //     "ultraSoundTask",
-    //     256,
-    //     NULL,
-    //     1,
-    //     uxUltraSoundTaskStack,
-    //     &xUltraSoundTaskTCB);  // Add the main task into the scheduler
+    xTaskCreateStatic(
+        ARTask, "ARTask", 256, NULL, 1, uxARTaskStack, &xARTaskTCB);
+    xTaskCreateStatic(
+        ultraSoundTask,
+        "ultraSoundTask",
+        256,
+        NULL,
+        1,
+        uxUltraSoundTaskStack,
+        &xUltraSoundTaskTCB);  // Add the main task into the scheduler
 
     /**
      * @todo Add your own task here
